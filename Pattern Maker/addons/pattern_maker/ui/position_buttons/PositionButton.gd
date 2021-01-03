85980a697842ea00a69f292462eea9478bfbfd80
Carlos Eduardo Perez Villanueva <carlospe097@outlook.com>
2021-01-02 20:36:11 -0600

    Pattern positions buttons now change their UI appearance according to the active position and spawned object type

--- Pattern Maker/addons/pattern_maker/ui/position_buttons/PositionButton.gd
+++ Pattern Maker/addons/pattern_maker/ui/position_buttons/PositionButton.gd
@@ -1,13 +1,70 @@
 tool
 extends VBoxContainer
 
+export(int, "Left", "Right", "Center", "Random") var position_value setget set_position_val
+
+signal changed_spawn_type
+
+const ACTIVE_COLOR: Color = Color(0.27, 0.57, 0.81)
+const OFFSET: Vector2 = Vector2(50, 20)
+const APPLE_ICON = preload("res://addons/pattern_maker/icons/apple_icon.png")
+const BOMB_ICON = preload("res://addons/pattern_maker/icons/bomb_icon.png")
+const NONE_ICON = preload("res://addons/pattern_maker/icons/none_dot_icon.png")
+
+var is_active: bool = false
+var current_spawn_item: String
+
 func _ready() -> void:
-	$Button/PopupMenu.add_item("Fruit")
-	$Button/PopupMenu.add_item("Bomb")
+	$PopupMenu.add_item("Fruit")
+	$PopupMenu.add_item("Bomb")
+
+func set_position_val(new_position: int) -> void:
+	if position_value != new_position:
+		match new_position:
+			0:
+				$Label.text = "Left"
+			1:
+				$Label.text = "Right"
+			2:
+				$Label.text = "Center"
+			3:
+				$Label.text = "Random"
+
+		position_value = new_position
+		update()
 
 func _on_Button_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		match event.button_index:
+			BUTTON_LEFT:
+				if not is_active:
+					is_active = true
+					current_spawn_item = "fruit"
+					$Button.modulate = ACTIVE_COLOR
+					update_spawn_item()
+					emit_signal("changed_spawn_type", current_spawn_item, position_value)
 			BUTTON_RIGHT:
-				$PopupMenu.set_as_toplevel(false)
-				$Button/PopupMenu.popup($Button.rect_global_position)
+				if is_active:
+					var pos = rect_global_position + OFFSET
+					$PopupMenu.popup(Rect2(pos, $PopupMenu.rect_size))
+
+func update_spawn_item() -> void:
+	if current_spawn_item == "fruit":
+		$Button.texture_normal = APPLE_ICON
+	elif current_spawn_item == "bomb":
+		$Button.texture_normal = BOMB_ICON
+
+func reset_values() -> void:
+	is_active = false
+	$Button.texture_normal = NONE_ICON
+	$Button.modulate = Color(1, 1, 1)
+
+func _on_PopupMenu_id_pressed(id: int) -> void:
+	if id == 0:
+		current_spawn_item = "fruit"
+		$Button.texture_normal = APPLE_ICON
+	elif id == 1:
+		current_spawn_item = "bomb"
+		$Button.texture_normal = BOMB_ICON
+
+	emit_signal("changed_spawn_type", current_spawn_item, position_value)
---
+++ Pattern Maker/addons/pattern_maker/ui/positions_row/PositionsRow.gd
@@ -0,0 +1,26 @@
+tool
+extends HBoxContainer
+
+func _ready() -> void:
+	for position_button in get_children():
+		position_button.connect("changed_spawn_type", self,
+				"_on_position_button_changed_spawn_type")
+
+func _on_position_button_changed_spawn_type(spawn_item: String,
+		position_value: int) -> void:
+#	match position_value:
+#			0: #left
+#				$Label.text = "Left"
+#			1: #right
+#				$Label.text = "Right"
+#			2: #center
+#				$Label.text = "Center"
+#			3: #random
+#				$Label.text = "Random"
+
+	for position_button in get_children():
+		if position_button.position_value != position_value:
+			position_button.reset_values()
+
+	print(spawn_item)
+	pass
